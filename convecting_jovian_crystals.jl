b7df21b1be3bb0d8b83f932aa323c330628edba2
Gregory Wagner <wagner.greg@gmail.com>
2022-06-17 13:28:52 -0400

    Update

--- convecting_jovian_crystals.jl
+++ convecting_jovian_crystals.jl
@@ -8,8 +8,8 @@ include("PolarPlaneCoriolis.jl")
 
 using .PolarPlaneCoriolis: PolarPlane
 
-Nh = 384
-Nz = 128
+Nh = 256
+Nz = 64
 architecture = GPU()
 f₀ = 3.52e-4
 γ = 7.87e-20
@@ -17,12 +17,15 @@ U = 80
 Lᵧ = (U / γ)^(1/3)
 L = 6Lᵧ
 H = Lᵧ / 5
-
-@show τᵧ = Lᵧ / U
+τᵧ = Lᵧ / U
 
 # τᵇ = (H^2 / Qᵇ)^(1/3)
-# -> Q = H^2 / τᵧ^3
-Qᵇ = 1e6 * H^2 / τᵧ^3
+# -> Q = R^3 * H^2 / τᵧ^3
+# where R = τᵇ / τᵧ
+Qᵇ = 1e-4 #1e-1 * H^2 / τᵧ^3
+
+w★ = (Qᵇ * H)^(1/3)
+@show Ro★ = w★ / (f₀ * H)
 
 grid = RectilinearGrid(architecture;
                        size = (Nh, Nh, Nz),
@@ -32,7 +35,7 @@ grid = RectilinearGrid(architecture;
                        topology = (Bounded, Bounded, Bounded))
 
 # Uncomment to mask out domain corners
-@inline circle(x, y, z) = sqrt(x^2 + y^2) > 0.98L
+@inline circle(x, y, z) = sqrt(x^2 + y^2) > 0.9L
 grid = ImmersedBoundaryGrid(grid, GridFittedBoundary(circle))
 
 b_top_bc = FluxBoundaryCondition(Qᵇ)
@@ -44,23 +47,29 @@ model = NonhydrostaticModel(; grid,
                             tracers = :b,
                             buoyancy = BuoyancyTracer(),
                             advection = WENO5(),
+                            closure = AnisotropicMinimumDissipation(),
+                            boundary_conditions = (; b=b_bcs),
                             coriolis = PolarPlane(; f₀, γ))
 
 u, v, w = model.velocities
 
-λ = L / 4
-ϵ(x, y, z) = 2 * (rand() - 1)
-ρ(x, y, z) = exp(-(x^2 + y^2) / 2λ^2)
-uᵢ(x, y, z) = U * ϵ(x, y, z) * ρ(x, y, z)
+wΔ = (Qᵇ * grid.Δzᵃᵃᶜ)^(1/3)
 
-set!(model, u=uᵢ, v=uᵢ)
+λ = L / 4
+ρ(x, y, z) = exp(-(x^2 + y^2) / 2λ^2)
+ϵ(x, y, z) = 2 * (rand() - 1)
+uᵢ(x, y, z) = U * ϵ(x, y, z) * ρ(x, y, z)
+wᵢ(x, y, z) = 1e-3 * wΔ * ϵ(x, y, z)
+
+set!(model, u=uᵢ, v=uᵢ, w=wᵢ)
 
 u, v, w = model.velocities
 u .-= mean(u)
 v .-= mean(v)
 
-Δt = 0.1 * grid.Δxᶜᵃᵃ / U
-simulation = Simulation(model; Δt, stop_time=60days)
+# Δt = 0.1 * grid.Δxᶜᵃᵃ / U
+Δt = 0.1 * grid.Δzᵃᵃᶜ / wΔ
+simulation = Simulation(model; Δt, stop_iteration=2000) #stop_time=10days)
 
 wall_time = Ref(time_ns())
 
@@ -80,7 +89,7 @@ end
 
 simulation.callbacks[:progress] = Callback(progress, IterationInterval(10))
 
-wizard = TimeStepWizard(cfl=0.5)
+wizard = TimeStepWizard(cfl=0.3)
 simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(10))
 
 u, v, w = model.velocities
@@ -114,22 +123,40 @@ simulation.output_writers[:slice] = JLD2OutputWriter(model, (; ω, u, v, w, b),
 
 run!(simulation)
 
-#=
-ωt = FieldTimeSeries(filename, "ω")
-wt = FieldTimeSeries(filename, "w")
+ωt = FieldTimeSeries(filename * "_mid.jld2", "ω")
+wt = FieldTimeSeries(filename * "_mid.jld2", "w")
+
+wxzt = FieldTimeSeries(filename * "_slice.jld2", "w")
+bxzt = FieldTimeSeries(filename * "_slice.jld2", "b")
+
+wlim = maximum(interior(wt[end], :, :, 1)) / 2
+ωlim = maximum(interior(ωt[end], :, :, 1)) / 2
+bmin = minimum(interior(bxzt[end], :, 1, :))
+bmax = maximum(interior(bxzt[end], :, 1, :))
 
 t = ωt.times
 Nt = length(t)
 
-fig = Figure()
+fig = Figure(resolution=(1600, 1200))
 ax1 = Axis(fig[1, 1], aspect=1)
 ax2 = Axis(fig[1, 2], aspect=1)
-slider = Slider(fig[2, 1:2], range=1:Nt, startvalue=1)
+
+ax3 = Axis(fig[2, 1], aspect=1)
+ax4 = Axis(fig[2, 2], aspect=1)
+
+slider = Slider(fig[3, 1:3], range=1:Nt, startvalue=1)
 n = slider.value
+
 ωⁿ = @lift interior(ωt[$n], :, :, 1)
 wⁿ = @lift interior(wt[$n], :, :, 1)
-heatmap!(ax1, ωⁿ)
-heatmap!(ax2, wⁿ)
+
+wxzⁿ = @lift interior(wxzt[$n], :, 1, :)
+bxzⁿ = @lift interior(bxzt[$n], :, 1, :)
+
+heatmap!(ax1, ωⁿ, colormap=:delta, colorrange=(-ωlim, ωlim))
+heatmap!(ax2, wⁿ, colormap=:delta, colorrange=(-wlim, wlim))
+
+heatmap!(ax3, wxzⁿ, colormap=:delta, colorrange=(-wlim, wlim))
+heatmap!(ax4, bxzⁿ, colorrange=(bmin, bmax))
 
 display(fig)
-=#
